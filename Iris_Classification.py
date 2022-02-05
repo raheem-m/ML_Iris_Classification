e92733d1cbab51ed9ec0fb96772aa46efb82ac38
raheem-m <77852752+raheem-m@users.noreply.github.com>
2022-02-04 23:31:09 -0500

    Add files via upload

---
+++ Iris_Classification.py
@@ -0,0 +1,87 @@
+#importing libraries
+import sys
+from this import d
+import scipy
+import numpy
+import matplotlib
+import pandas
+import sklearn
+
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+#data summaries
+print (dataset.shape)
+print (dataset.head(20))
+print (dataset.describe())
+print (dataset.groupby('class').size())
+
+#data visualization
+#box and whisket plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+#histogram: used to look at distribution to assume algorithms to use
+dataset.hist()
+pyplot.show
+#scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+
+#Splitting out data sets for a validation set
+array = dataset.values
+X = array[:,0:4]
+y = array [:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+#spot check algorithms
+models = []
+models.append (('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append (('LDA',LinearDiscriminantAnalysis()))
+models.append (('KNN', KNeighborsClassifier()))
+models.append (('CART',DecisionTreeClassifier()))
+models.append (('NB', GaussianNB()))
+models.append (('SVM',SVC(gamma='auto')))
+
+#evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle = True)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append (name)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+#algorithm comparison visualized
+pyplot.boxplot(results, labels=names)
+pyplot.title('algorithm comparison')
+pyplot.show()
+#SVC DEEMED MOST ACCURATE
+
+#Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train,Y_train)
+predictions = model.predict(X_validation)
+
+#evaluate predictions
+print (accuracy_score(Y_validation, predictions))
+print (confusion_matrix(Y_validation, predictions))
+print (classification_report(Y_validation, predictions))
